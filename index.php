53addf8976c99c284d8e99f17a4ab50fdfdcfa86
fcrios145 <fcrios145@hotmail.com>
2013-10-18 20:56:12 -0600

    subo la estructura del proyecto al repositorio

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+	<title>Soluciones Medicas Integrales</title>
+	<link rel="stylesheet" href="css/reseter.css" type="text/css">
+	<link rel="stylesheet" href="css/estilo.css" type="text/css">
+
+</head>
+<body>
+
+
+	<div id="wrapper">
+		<div id="header">
+			<span>Tel. (6691) 12 02 39</span>
+			<span>email: solumedi1@gmail.com</span>
+			<img src="img/logo.png" alt="soluciones medicas integrales">
+			<ul>
+
+				<a href="#"><li>HOME</li></a>
+				<a href="#"><li>ESPECIALIDADES</li></a>
+				<a href="#"><li>SERVICIOS</li></a>
+				<a href="#"><li>CITAS</li></a>
+				<a href="#"><li>¿QUIENES SOMOS?</li></a>
+				
+			</ul>
+		</div>
+		<div id="content">
+			<img src="" alt="servicio1"> <span>some text</span>
+			<img src="" alt="servicio2"> <span>Some text</span>
+			<img src="" alt="servicio3"> <span>some text</span>
+		</div>
+
+		
+
+
+		<div id="footer">
+			<ul>
+				<a href="#"><li>LINK1</li></a>
+				<a href="#"><li>LINK2</li></a>
+				<a href="#"><li>LINK3</li></a>
+				<a href="#"><li>LINK4</li></a>
+				<a href="#"><li>LINK5</li></a>
+			</ul>
+
+			img/foto1.jpg
+
+			<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=Taller+Mec%C3%A1nico+Industrial+Guerrero&amp;aq=t&amp;sll=37.0625,-95.677068&amp;sspn=39.184175,56.513672&amp;ie=UTF8&amp;hq=Taller+Mec%C3%A1nico+Industrial+Guerrero&amp;hnear=&amp;ll=23.222841,-106.391185&amp;spn=0.071946,0.071946&amp;t=m&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=es&amp;geocode=&amp;q=Taller+Mec%C3%A1nico+Industrial+Guerrero&amp;aq=t&amp;sll=37.0625,-95.677068&amp;sspn=39.184175,56.513672&amp;ie=UTF8&amp;hq=Taller+Mec%C3%A1nico+Industrial+Guerrero&amp;hnear=&amp;ll=23.222841,-106.391185&amp;spn=0.071946,0.071946&amp;t=m" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small>
+
+			<img src="" alt="facebookLogo">
+			<img src="" alt="twitterLogo">
+
+		</div>
+
+	</div>	
+
+</body>
+</html>
